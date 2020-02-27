12476867ad436e34c4dc146bbd37a9cafc0f9844
naresh <nareshchoudhary612@gmail.com>
2020-02-27 03:15:28 -0600

    chap2.05

--- src/main/java/com/frankmoley/security/app/ApplicationSecurityConfiguration.java
+++ src/main/java/com/frankmoley/security/app/ApplicationSecurityConfiguration.java
@@ -1,23 +1,66 @@
 package com.frankmoley.security.app;
 
+import com.frankmoley.security.app.auth.LandonUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true) // this would allow method level security ( we added role -> user, admin in methods of controller
 public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter{
+    @Autowired
+    private LandonUserDetailsService userDetailsService;
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(11));
+        provider.setAuthoritiesMapper(authoritiesMapper());
+        return provider;
+    }
+    
+    // AuthoritiesMapper takes values from database and prefixes  ROLE_  
+    @Bean
+    public GrantedAuthoritiesMapper authoritiesMapper(){
+        SimpleAuthorityMapper authorityMapper = new SimpleAuthorityMapper();
+        authorityMapper.setConvertToUpperCase(true);
+        authorityMapper.setDefaultAuthority("USER");
+        return authorityMapper;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //Below settings allowed anyone to access index page but view guest needed authentication
-    	http  // call to http builder
-                .csrf().disable()	// to prevent csrf exploit
+        http
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/index", "/css/*", "/js/*").permitAll()  // root, indexpage allowed, any static css or javascript pages
-                .anyRequest().authenticated() // right now authenticate every request
+                .antMatchers("/", "/index", "/css/*", "/js/*").permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .httpBasic();
     }
+
 }
